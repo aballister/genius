061f28333ad4f15edc5dd8e0b46baea4fb9963de
Oleksii Khryniuk <Aleksey.Khrynyuk@exist.ua>
2026-01-20 21:44:30 +0200

    add banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -5,12 +5,6 @@ export default function Banner() {
     return (
         <div className={styles.banner}>
             <div className={styles.container}>
-                <div className={styles.wrapper}>
-                    <div className={styles.subtitle}>health clinic</div>
-                    <h1 className={styles.title}>GENIUS</h1>
-                    <div className={styles.slogan}>Ваше здоров&apos;я – наш пріоритет</div>
-                    <BannerButton />
-                </div>
             </div>
         </div>
     )
